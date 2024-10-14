f7e1885fc394d3311dee40555955ad753ada1e91
Rodrigo <rofen1@protonmail.com>
2024-10-14 13:23:45 -0300

    Lista 02 Ex 05

---
+++ lista-02/src/Ex05.java
@@ -0,0 +1,10 @@
+public class Ex05 {
+    public static void main(String[] args) {
+        for(int i=1; i < 10; i++) {
+            for (int j=1; j <= 10; j++) {
+                System.out.printf("%2d x %2d = %2d\n", i, j, i * j);
+            }
+            System.out.println();
+        }
+    }
+}
