1a74bf594763bf9e9d947b7d35b87152c35f315d
Rodrigo <rofen1@protonmail.com>
2024-10-14 13:15:04 -0300

    Lista 02 Ex 03

--- lista-02/src/Ex03.java
+++ lista-02/src/Ex03.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Ex03 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ano;
+
+        System.out.print("Informe um ano: ");
+        ano = scanner.nextInt();
+
+        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+            System.out.println("O ano é bissexto");
+        } else {
+            System.out.println("O ano não é bissexto");
+        }
+
+        scanner.close();
+    }
 }
