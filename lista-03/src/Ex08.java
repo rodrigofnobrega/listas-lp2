96ec22f9cd814ce5eb11fceab77e6a1258f23667
Rodrigo <rofen1@protonmail.com>
2024-10-14 14:54:28 -0300

    Lista 03 Ex 08

---
+++ lista-03/src/Ex08.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ex08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matriz = new int[3][3];
+        int soma = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("Informe o valor da posição [%d][%d]: ", i, j);
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < i; j++) {
+                soma += matriz[i][j];
+            }
+        }
+
+        System.out.println("Imprimindo a matriz");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%2d ", matriz[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("Soma dos elementos: " + soma);
+
+        scanner.close();
+    }
+}
